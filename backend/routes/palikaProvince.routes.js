a385fe40b97d071a63ecd924335b701d6375faa4
Ceonc <112940056+Ceonc@users.noreply.github.com>
2022-09-06 15:33:30 +0545

    Delete backend directory

--- backend/routes/palikaProvince.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-const palikaProvinceController = require('../controllers/palikaProvince.controller')
-
-router.get("/", palikaProvinceController.PalikaProvince)
-router.get("/robson", palikaProvinceController.PalikaProvinceRobson)
-
-module.exports = { palikaProvinceRouter: router }
